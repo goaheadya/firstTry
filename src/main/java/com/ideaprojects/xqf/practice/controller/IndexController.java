c4d226a9c501a6c88a3c3be5b7d2e899a9dcf737
go_ahead <1921576767@qq.com>
2022-02-25 20:40:38 +0800

    check cookies

--- src/main/java/com/ideaprojects/xqf/practice/controller/IndexController.java
+++ src/main/java/com/ideaprojects/xqf/practice/controller/IndexController.java
@@ -1,10 +1,32 @@
 package com.ideaprojects.xqf.practice.controller;
 
+import com.ideaprojects.xqf.practice.mapper.UserMapper;
+import com.ideaprojects.xqf.practice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller //注解
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() { return "index"; }
+    public String index(HttpServletRequest requst) {
+        Cookie[] cookies = requst.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    requst.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        return "index";
+    }
 }
